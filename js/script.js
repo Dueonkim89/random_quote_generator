1476630cfc5cd237c4a74e857b0c3c4db96814da
Dueon Kim <dueonkim89@yahoo.com>
2017-08-23 19:54:00 -0700

    disable autoGenerate option created

--- js/script.js
+++ js/script.js
@@ -18,7 +18,10 @@ citation: 'The Pursuit of Excellence', year: 1994, category: 'inspiration' }
 //array to log previous random numbers
 var randomNumberList = [];
 
-//array to log the delays
+// undefined variable to store the delayed quotes
+var delayedQuotes;
+
+//array to push the delayed auto-generated quotes
 var numberOfDelays = [];
 
 //array for background colors
@@ -32,7 +35,7 @@ var quote = document.getElementsByClassName('quote')[0];
 var source = document.getElementsByClassName('source')[0];
 
 
-//function to return an original quote. (no duplicates)
+//function to return an original quote. (no duplicates unless count is over 6)
 function getRandomQuote(count) {
 	return quotes[randomNumberList[count]];
 }
@@ -42,42 +45,45 @@ function printQuote(count) {
 	// set bodybackground color based off number
 	document.body.style.backgroundColor = backGroundColorList[randomNumberList[count]];
 	//set the quote up.
-	quote.innerText = quotes[randomNumberList[count]].quote;
-	source.innerText = '-' + quotes[randomNumberList[count]].source + ',';
+	quote.innerText = referenceNumber.quote;
+	source.innerText = '-' + referenceNumber.source + ',';
 	var year = document.createElement("span"); 
 	year.className = 'year';
 	source.appendChild(year);
-	year.innerText = ' ' + quotes[randomNumberList[count]].year;	
-	if (quotes[randomNumberList[count]].citation !== undefined ) {
+	year.innerText = ' ' + referenceNumber.year;	
+	if (referenceNumber.citation !== undefined ) {
 		var citation = document.createElement("span"); 
 		citation.className = 'citation';	
-		citation.innerText = ' ' + quotes[randomNumberList[count]].citation + ',';
+		citation.innerText = ' ' + referenceNumber.citation + ',';
 		source.insertBefore(citation, year);
 	}	
 }
 
+//auto generate quotes after 5 secs.
 function autoGenerate(count) {
 	count += 1;
 	//named function declaration for the set time out. 5 sec delay per quote.
 	function setDelay(count, autoTimer) {
-		setTimeout(function(){
-			printQuote(count);
-		}, 5000 * autoTimer);		
+		delayedQuotes = setTimeout(function(){
+							printQuote(count);
+						}, 5000 * autoTimer);	
+		numberOfDelays.push(delayedQuotes);					
 	}
 	
 	//if count less than 7
 	if (count < 7) {
 		//for loop until we get to 6. increment the autoTimer and the count
-		for (i = count, autoTimer = 1; i < 7; i++, autoTimer++) {
-			numberOfDelays.push(setDelay(i, autoTimer)); 
+		for (count, autoTimer = 1; count < 7; count++, autoTimer++) {
+			setDelay(count, autoTimer);
 		}
+		// change global var count to 7. This is to make distinction between count parameter and global var count.
+		window.count = 7;	
 	// else 20 random quotes.	
 	} else {
 		for (i = 0, autoTimer = 1; i < 20; i++, autoTimer++) {
 			setDelay(Math.floor((Math.random() * quotes.length)), autoTimer); 
 		}
 	}
-	console.log(numberOfDelays);
 }
 
 
@@ -93,16 +99,17 @@ window.onload = function drawNumbers() {
 	}
 	// KEEP IT DRY!!, count starts at 0. 
 	printQuote(count);
-}
+};
 
 //delegate event handler to respond to the different button click events.
 document.addEventListener("click", function(event) {
 	var loadQuoteButton = document.getElementById('loadQuote');
 	if (event.target.id === 'disableAutomate') {
-		//cancel set time out if disabled button clicked.
-		//	clearInterval(setAutomation);
-		
-		
+		//cancel all the set time outs if disabled button clicked.
+		for (var i = 0; i < numberOfDelays.length; i++) {
+				clearTimeout(numberOfDelays[i]);
+		}
+		numberOfDelays.length = 0;				
 		//sync button settings
 		loadQuoteButton.style.display = '';
 		document.getElementById('disableAutomate').innerText = 'Auto-Generate Quote';
@@ -113,7 +120,7 @@ document.addEventListener("click", function(event) {
 		document.getElementById('automateQuote').innerText = 'Disable Auto-Generate';
 		document.getElementById('automateQuote').id = 'disableAutomate';	
 		//invoke the autoGenerate function.
-		autoGenerate(count);		
+		autoGenerate(count);
 	} else if (event.target.id === 'loadQuote') {
 		count += 1;
 		//ternary operator, if count greater or equal to 7, run random number, else run order of count
